fbf1623af252fc3ad2c30f55fe0ddefd9028aaf3
Antoine Codogno <toniocodo@hotmail.com>
2022-07-20 11:32:37 +0200

    feat: add json view for theme (#6)
    * feat: add json view for theme
    
    * chore: allow for tools/storybook

--- libs/tools/storybook/src/index.ts
+++ libs/tools/storybook/src/index.ts
@@ -1,4 +1,5 @@
 export * from './components/IconsView';
+export * from './components/JsonView';
 export * from './components/LoremIpsum';
 export * from './components/ToggleView';
 export * from './decorators/IntlDecorator';
