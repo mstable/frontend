633e5508e967e2078bef24f95fe956fd3c6fb97b
toniocodo <toniocodo@hotmail.com>
2022-08-29 13:47:10 +0200

    refactor: remove useless wrapper, extract constants

--- libs/shared/prices/src/state.ts
+++ libs/shared/prices/src/state.ts
@@ -1,6 +1,17 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
+import {
+  coingeckoCoinIds,
+  coingeckoEndpoint,
+} from '@frontend/shared-constants';
+import { axiosInstance } from '@frontend/shared-data-access';
+import { usePushNotification } from '@frontend/shared-notifications';
+import { useQuery } from '@tanstack/react-query';
+import produce from 'immer';
+import { path } from 'ramda';
+import { useIntl } from 'react-intl';
 import { createContainer } from 'react-tracked';
+import { useBlockNumber, useNetwork } from 'wagmi';
 
 import type { Children } from '@frontend/shared-utils';
 import type { Dispatch, SetStateAction } from 'react';
@@ -17,10 +28,49 @@ export const { Provider, useUpdate, useTrackedState } = createContainer<
   PricesState,
   Dispatch<SetStateAction<PricesState>>,
   Children
->(() =>
-  useState({
+>(() => {
+  const [state, setState] = useState<PricesState>({
     currency: 'usd',
     symbol: '$',
     price: null,
-  }),
-);
+  });
+  const intl = useIntl();
+  const { data: blockNumber } = useBlockNumber({ watch: true });
+  const pushNotification = usePushNotification();
+  const { chain } = useNetwork();
+  const coingeckoCoinId = useMemo(
+    () => coingeckoCoinIds[chain?.id] || 'ethereum',
+    [chain?.id],
+  );
+  useQuery(
+    ['prices', blockNumber, state.currency, chain?.id],
+    () =>
+      axiosInstance.get(
+        `${coingeckoEndpoint}/simple/price?ids=${coingeckoCoinId}&vs_currencies=${state.currency}`,
+      ),
+    {
+      staleTime: 0,
+      cacheTime: 0,
+      onError: (err: Error) => {
+        pushNotification({
+          title: intl.formatMessage({
+            defaultMessage: 'Error fetching prices',
+          }),
+          message: err?.message,
+          severity: 'error',
+        });
+      },
+      onSuccess: (data) => {
+        setState(
+          produce((draft) => {
+            draft.price = parseFloat(
+              path(['data', coingeckoCoinId, state.currency], data),
+            );
+          }),
+        );
+      },
+    },
+  );
+
+  return [state, setState];
+});
