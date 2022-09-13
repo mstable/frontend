43f61757ac19f35daf818a72194ace3464566816
toniocodo <toniocodo@hotmail.com>
2022-09-13 12:24:09 +0200

    chore: update env vars and access

--- libs/shared/data-access/codegen.js
+++ libs/shared/data-access/codegen.js
@@ -1,14 +1,12 @@
 module.exports = {
   generates: {
     'libs/shared/data-access/src/generated/graphql.ts': {
-      schema:
-        'https://api.studio.thegraph.com/query/32034/mstable-metavault-goerli/v0.1.0',
+      schema: process.env['NX_THE_GRAPH_MV_GOERLI_URL'],
       documents: ['**/src/**/*.graphql'],
       plugins: ['typescript'],
     },
     '**/src': {
-      schema:
-        'https://api.studio.thegraph.com/query/32034/mstable-metavault-goerli/v0.1.0',
+      schema: process.env['NX_THE_GRAPH_MV_GOERLI_URL'],
       documents: ['**/src/**/*.graphql'],
       preset: 'near-operation-file',
       presetConfig: {
