433f4649901c1248b84c5887f25ce5136398dc29
toniocodo <toniocodo@hotmail.com>
2022-09-19 09:42:14 +0200

    style: scale image

--- libs/mstable/meta-vault/src/components/Strategy/components/VisualizeStrategyDialog.tsx
+++ libs/mstable/meta-vault/src/components/Strategy/components/VisualizeStrategyDialog.tsx
@@ -7,7 +7,7 @@ import type { DialogOptions } from '@frontend/shared-modals';
 export const VisualizeStrategyDialog: DialogOptions = {
   maxWidth: 'xl',
   content: (
-    <Box p={2} sx={{ img: { width: 1 } }}>
+    <Box p={2} sx={{ img: { width: 1, height: 'auto', objectFit: 'contain' } }}>
       <img src={mvStrat} alt="Strategy" />
     </Box>
   ),
