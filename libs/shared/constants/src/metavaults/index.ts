27e9d0bd720f6f4507f6925bded549b8f164731a
toniocodo <toniocodo@hotmail.com>
2022-09-30 01:25:04 +0200

    feat: add test metavault WIP 🚧

--- libs/shared/constants/src/metavaults/index.ts
+++ libs/shared/constants/src/metavaults/index.ts
@@ -1,2 +1,23 @@
+import { chainId } from 'wagmi';
+
+import { test } from './test';
+import { usdc3crv } from './usdc3crv';
+
+import type { Metavault, SupportedMetavault } from './types';
+
 export * from './usdc3crv';
 export * from './types';
+
+export const supportedMetavaults: Record<
+  number,
+  Record<SupportedMetavault, Metavault>
+> = {
+  [chainId.mainnet]: {
+    usdc3crv: usdc3crv[chainId.mainnet],
+    test: test[chainId.mainnet],
+  },
+  [chainId.goerli]: {
+    usdc3crv: usdc3crv[chainId.goerli],
+    test: test[chainId.goerli],
+  },
+};
