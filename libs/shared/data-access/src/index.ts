553b25632d016bb0ce6d01498e2f8a739bcc1985
Calvin Kei <calvin.conceptz@gmail.com>
2022-10-13 14:36:01 +0900

    fix: remove useless code

--- libs/shared/data-access/src/index.ts
+++ libs/shared/data-access/src/index.ts
@@ -1,5 +1,4 @@
 export * from './clients/axios';
-export * from './clients/graphql';
 export * from './clients/react-query';
 export * from './generated/graphql';
 export * from './hooks/useDataSource';
