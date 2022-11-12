90805741ad7f8e37411b8447d24150d26bfa64fc
toniocodo <toniocodo@hotmail.com>
2022-11-12 01:06:09 +0100

    fix: icon sizes

--- libs/shared/icons/src/components/meta-vaults/index.tsx
+++ libs/shared/icons/src/components/meta-vaults/index.tsx
@@ -8,14 +8,14 @@ import { ReactComponent as MvUSDCSvg } from './mvUSDC.svg';
 import type { SvgIconProps } from '@mui/material';
 
 export const MvETH = (props: SvgIconProps) => (
-  <SvgIcon {...props} component={MvETHSvg} viewBox="8 8.52 48 48" />
+  <SvgIcon {...props} component={MvETHSvg} viewBox="0 0 64 65" />
 );
 export const MvFrax = (props: SvgIconProps) => (
-  <SvgIcon {...props} component={MvFraxSvg} viewBox="8.33 8.52 48 48" />
+  <SvgIcon {...props} component={MvFraxSvg} viewBox="0 0 65 65" />
 );
 export const MvMUSD = (props: SvgIconProps) => (
-  <SvgIcon {...props} component={MvMUSDSvg} viewBox="20 20 80 80" />
+  <SvgIcon {...props} component={MvMUSDSvg} viewBox="0 0 120 120" />
 );
 export const MvUSDC = (props: SvgIconProps) => (
-  <SvgIcon {...props} component={MvUSDCSvg} viewBox="8 8 64 64" />
+  <SvgIcon {...props} component={MvUSDCSvg} viewBox="0 0 80 80" />
 );
