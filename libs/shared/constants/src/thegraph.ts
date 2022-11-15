86101a5040722f513e5150eddf098ce1c44674ce
toniocodo <toniocodo@hotmail.com>
2022-11-15 19:01:12 +0100

    fix: generate types, remove TODO

--- libs/shared/constants/src/thegraph.ts
+++ libs/shared/constants/src/thegraph.ts
@@ -1,6 +1,5 @@
 import { chainId } from 'wagmi';
 
-// TODO update endpoints when available
 export const metaVaultEndpoints: Record<number, string> = {
   [chainId.mainnet]: process.env['NX_THE_GRAPH_MV_MAINNET_URL'],
   [chainId.goerli]: process.env['NX_THE_GRAPH_MV_GOERLI_URL'],
