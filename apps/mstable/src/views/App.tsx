86bb65c21989737fd2f68ccfa70aa48a1b18c3d2
toniocodo <toniocodo@hotmail.com>
2022-11-15 19:17:36 +0100

    refactor: move context composition to main

--- apps/mstable/src/views/App.tsx
+++ apps/mstable/src/views/App.tsx
@@ -1,36 +1,13 @@
-import { SettingsProvider } from '@frontend/mstable-settings';
 import { Topnav } from '@frontend/mstable-shared-ui';
-import { GasFeeProvider } from '@frontend/shared-gas-fee';
-import { I18nProvider } from '@frontend/shared-i18n';
-import { ModalsProvider } from '@frontend/shared-modals';
-import { NotificationsProvider } from '@frontend/shared-notifications';
-import { PricesProvider } from '@frontend/shared-prices';
-import { ThemeProvider } from '@frontend/shared-theme';
 import { ErrorBoundary, ErrorPage } from '@frontend/shared-ui';
-import { composeContexts } from '@frontend/shared-utils';
-import {
-  ChainRefresherProvider,
-  useUnsupportedNetworks,
-  WagmiProvider,
-} from '@frontend/shared-wagmi';
+import { useUnsupportedNetworks } from '@frontend/shared-wagmi';
 import { Stack } from '@mui/material';
-import { Outlet, Router } from '@tanstack/react-location';
-import { QueryClientProvider } from '@tanstack/react-query';
+import { Outlet } from '@tanstack/react-location';
 import { useEffectOnce } from 'react-use';
 
-import en from '../assets/lang/en.json';
-import {
-  chains,
-  reactLocation,
-  reactQueryClient,
-  registerCharts,
-  wagmiClient,
-} from '../clients';
-import { routes } from '../routes';
+import { registerCharts } from '../clients';
 
-import type { Theme } from '@mui/material';
-
-const AppWrapped = () => {
+export const App = () => {
   useUnsupportedNetworks();
 
   useEffectOnce(() => {
@@ -60,33 +37,3 @@ const AppWrapped = () => {
     </Stack>
   );
 };
-
-export const App = () =>
-  composeContexts(
-    [
-      [QueryClientProvider, { client: reactQueryClient }],
-      [I18nProvider, { messages: { en } }],
-      [ThemeProvider],
-      [Router, { location: reactLocation, routes }],
-      [
-        NotificationsProvider,
-        {
-          containerProps: {
-            sx: {
-              top: (theme: Theme) => theme.spacing(10),
-              right: (theme: Theme) => theme.spacing(2),
-              left: undefined,
-              bottom: undefined,
-            },
-          },
-        },
-      ],
-      [WagmiProvider, { client: wagmiClient, chains: chains }],
-      [ChainRefresherProvider],
-      [PricesProvider],
-      [GasFeeProvider],
-      [ModalsProvider],
-      [SettingsProvider],
-    ],
-    <AppWrapped />,
-  );
