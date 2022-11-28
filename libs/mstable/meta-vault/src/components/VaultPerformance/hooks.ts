1c63338bd8ac6111c65bec3d3ab185009505c935
toniocodo <toniocodo@hotmail.com>
2022-11-28 20:41:25 +0100

    feat: compute min max with range on charts

--- libs/mstable/meta-vault/src/components/VaultPerformance/hooks.ts
+++ libs/mstable/meta-vault/src/components/VaultPerformance/hooks.ts
@@ -15,29 +15,50 @@ import type { ChartData, ChartOptions, ScriptableContext } from 'chart.js';
 
 import type { ChartTimeframe, ChartType } from './types';
 
+type Serie = { label: string; value: any };
+
+const getRange = (margin: number, series: Serie[]) =>
+  !isNilOrEmpty(series)
+    ? series.reduce(
+        (acc, curr) => ({
+          min: Math.min(acc.min, Number(curr.value) - margin),
+          max: Math.max(acc.max, Number(curr.value) + margin),
+        }),
+        {
+          min: series[0].value,
+          max: series[0].value,
+        },
+      )
+    : {
+        min: undefined,
+        max: undefined,
+      };
+
 export const useChartConfig = () => {
   const intl = useIntl();
   const mv = useMetavault();
-
   const chartTypes = {
     APS: {
       id: 'APS' as ChartType,
       label: intl.formatMessage({ defaultMessage: 'Share Price' }),
+      chartMargin: 0.001,
       getValue: (v) => v.assetPerShare,
       getLabel: (v) => intl.formatNumber(v, { style: 'decimal' }),
     },
-    // APY: {
-    //   id: 'APY' as ChartType,
-    //   label: intl.formatMessage({ defaultMessage: 'APY' }),
-    //   getValue: (v) => Number(v.apy),
-    //   getLabel: (v) => (Number(v) * 100).toFixed(2) + '%',
-    // },
+    APY: {
+      id: 'APY' as ChartType,
+      label: intl.formatMessage({ defaultMessage: 'APY' }),
+      chartMargin: 0.001,
+      getValue: (v) => Number(v.apy),
+      getLabel: (v) => (Number(v) * 100).toFixed(2) + '%',
+    },
     TVL: {
       id: 'TVL' as ChartType,
       label: intl.formatMessage(
         { defaultMessage: 'TVL ({symbol})' },
         { symbol: mv?.assetToken?.symbol },
       ),
+      chartMargin: 1e3,
       getValue: (v) =>
         new BigDecimal(v.totalAssets, mv?.assetToken?.decimals).simple,
       getLabel: (v) => intl.formatNumber(v, { notation: 'compact' }),
@@ -126,35 +147,14 @@ export const useChartData = (
         : [],
     [assetToken, chartType, chartTypes, data?.vault?.DailyVaultStats],
   );
-
-  const min = useMemo(
+  const minMax = useMemo(
     () =>
       ({
-        APS: !isNilOrEmpty(series)
-          ? series.reduce(
-              (acc, curr) => Math.min(acc, Number(curr.value) - 0.005),
-              series[0].value,
-            )
-          : undefined,
-        APY: Math.min(0, ...series.map((d) => d.value)),
-        TVL: Math.min(0, ...series.map((d) => d.value)),
+        APS: getRange(chartTypes[chartType].chartMargin, series),
+        APY: getRange(chartTypes[chartType].chartMargin, series),
+        TVL: getRange(chartTypes[chartType].chartMargin, series),
       }[chartType]),
-    [chartType, series],
-  );
-
-  const max = useMemo(
-    () =>
-      ({
-        APS: !isNilOrEmpty(series)
-          ? series.reduce(
-              (acc, curr) => Math.max(acc, Number(curr.value) + 0.005),
-              series[0].value,
-            )
-          : undefined,
-        APY: undefined,
-        TVL: undefined,
-      }[chartType]),
-    [chartType, series],
+    [chartType, chartTypes, series],
   );
 
   const chartData: { data: ChartData<'line'>; options: ChartOptions<'line'> } =
@@ -190,8 +190,8 @@ export const useChartData = (
                 color: theme.palette.divider,
                 drawBorder: false,
               },
-              min,
-              max,
+              min: minMax?.min,
+              max: minMax?.max,
               ticks: {
                 callback: chartTypes[chartType].getLabel,
                 color: theme.palette.text.secondary,
@@ -261,8 +261,8 @@ export const useChartData = (
         theme.typography.value5.fontFamily,
         theme.typography.value5.fontSize,
         theme.typography.value5.fontWeight,
-        min,
-        max,
+        minMax?.min,
+        minMax?.max,
       ],
     );
 
