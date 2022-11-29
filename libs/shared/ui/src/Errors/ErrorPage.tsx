48137a8d406eac1fe5e08a57c47b7b6ed66cc2e4
toniocodo <toniocodo@hotmail.com>
2022-11-29 13:31:20 +0100

    feat: add Tips and styles to error page 🚧WIP

--- libs/shared/ui/src/Errors/ErrorPage.tsx
+++ libs/shared/ui/src/Errors/ErrorPage.tsx
@@ -1,8 +1,19 @@
 import { DISCORD_SUPPORT } from '@frontend/shared-constants';
-import { Link, Stack, Typography } from '@mui/material';
+import {
+  Divider,
+  Link,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import { Browsers, WifiX } from 'phosphor-react';
 import { useIntl } from 'react-intl';
 
-import type { StackProps } from '@mui/material';
+import type { PaletteMode, StackProps } from '@mui/material';
 import type { ReactNode } from 'react';
 
 export type ErrorPageProps = {
@@ -12,6 +23,11 @@ export type ErrorPageProps = {
   hideSupport?: boolean;
 } & StackProps;
 
+const colorGradient = (mode: PaletteMode) =>
+  mode === 'light'
+    ? `linear-gradient(90deg, #D1C6FF 2.1%, #6A67CB 27.6%)`
+    : `linear-gradient(90deg, #D1C6FF 2.1%, #6A67CB 27.6%)`;
+
 export const ErrorPage = ({
   title,
   subtitle,
@@ -20,46 +36,101 @@ export const ErrorPage = ({
   ...rest
 }: ErrorPageProps) => {
   const intl = useIntl();
+  const theme = useTheme();
 
   return (
-    <Stack direction="column" py={18} alignItems="flex-start" {...rest}>
-      <Typography variant="h1" py={4}>
-        {title ??
-          intl.formatMessage({
-            defaultMessage: 'Unhandled Error!',
+    <Stack direction="row" py={18} {...rest}>
+      <Stack direction="column" alignItems="flex-start" width={1 / 2}>
+        <Typography
+          py={4}
+          sx={(theme) => ({
+            fontSize: 64,
+            fontWeight: 800,
+            lineHeight: '64px',
+            background: colorGradient(theme.palette.mode),
+            backgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
           })}
-      </Typography>
-      <Typography variant="h4" pb={8}>
-        {subtitle ??
-          intl.formatMessage({
-            defaultMessage:
-              'There was an error while loading this page, you can try to reload.',
-          })}
-      </Typography>
-      {message}
-      {!hideSupport && (
-        <Typography variant="label2">
-          {intl.formatMessage(
-            {
-              defaultMessage:
-                'If the problem persists, you can contact us through our {support}.',
-            },
-            {
-              support: (
-                <Link
-                  href={DISCORD_SUPPORT}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {intl.formatMessage({
-                    defaultMessage: 'Discord support channel',
-                  })}
-                </Link>
-              ),
-            },
-          )}
+        >
+          {title ??
+            intl.formatMessage({
+              defaultMessage: 'Unhandled Error!',
+            })}
         </Typography>
-      )}
+        <Typography variant="h4" pb={8} color="grey.600">
+          {subtitle ??
+            intl.formatMessage({
+              defaultMessage: 'Oops, something went wrong.',
+            })}
+        </Typography>
+        {message}
+        {!hideSupport && (
+          <Typography variant="label2">
+            {intl.formatMessage(
+              {
+                defaultMessage:
+                  'If the problem persists, you can contact us through our {support}.',
+              },
+              {
+                support: (
+                  <Link
+                    href={DISCORD_SUPPORT}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {intl.formatMessage({
+                      defaultMessage: 'Discord support channel',
+                    })}
+                  </Link>
+                ),
+              },
+            )}
+          </Typography>
+        )}
+      </Stack>
+      <Stack direction="column" width={1 / 2} p={4}>
+        <Stack
+          borderRadius={1}
+          bgcolor="background.paper"
+          alignItems="stretch"
+          border={(theme) => `1px solid ${theme.palette.divider}`}
+          p={2}
+        >
+          <Typography variant="h4" color="grey.600">
+            {intl.formatMessage({
+              defaultMessage: 'Tips',
+            })}
+          </Typography>
+          <List>
+            <ListItem>
+              <ListItemIcon>
+                <Browsers color={theme.palette.icons.color} size={24} />
+              </ListItemIcon>
+              <ListItemText
+                primary={intl.formatMessage({ defaultMessage: 'Check URL' })}
+                secondary={intl.formatMessage({
+                  defaultMessage: 'Make sure you are on the right URL.',
+                })}
+              />
+            </ListItem>
+            <Divider />
+            <ListItem>
+              <ListItemIcon>
+                <WifiX color={theme.palette.icons.color} size={24} />
+              </ListItemIcon>
+              <ListItemText
+                primary={intl.formatMessage({
+                  defaultMessage: 'Check connectivity',
+                })}
+                secondary={intl.formatMessage({
+                  defaultMessage:
+                    'Make sure your internet connection is working.',
+                })}
+              />
+            </ListItem>
+          </List>
+        </Stack>
+      </Stack>
     </Stack>
   );
 };
