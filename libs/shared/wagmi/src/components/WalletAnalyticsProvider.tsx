34c11437c92b74a6fb00a953011c83ab4d5de128
toniocodo <toniocodo@hotmail.com>
2022-12-02 09:58:38 +0100

    feat: filter out autoconnect

--- libs/shared/wagmi/src/components/WalletAnalyticsProvider.tsx
+++ libs/shared/wagmi/src/components/WalletAnalyticsProvider.tsx
@@ -9,22 +9,27 @@ import type { Children } from '@frontend/shared-utils';
 export const WalletAnalyticsProvider = ({ children }: Children) => {
   const track = useTrack();
   const { chain } = useNetwork();
-  const { address, status } = useAccount();
+  const { address, status, connector } = useAccount();
   const prevAddress = usePrevious(address);
 
   useEffect(() => {
-    if (status === 'connected') {
-      track('wallet_connect', { address, chain: chain?.id });
-    } else if (!!prevAddress && status === 'disconnected') {
+    if (!!prevAddress && status === 'disconnected') {
       track('wallet_disconnect', { address: prevAddress });
     } else if (!!prevAddress && !!address && prevAddress !== address) {
       track('wallet_switch', {
         address,
         prevAddress,
         chain: chain?.id,
+        wallet: connector?.name,
+      });
+    } else if (!prevAddress && !!address && status === 'connected') {
+      track('wallet_connect', {
+        address,
+        wallet: connector?.name,
+        chain: chain?.id,
       });
     }
-  }, [address, chain?.id, prevAddress, status, track]);
+  }, [address, chain?.id, connector?.name, prevAddress, status, track]);
 
   return <>{children}</>;
 };
