d663674f1be71bb97118e4cd67f51b0ece9ff43e
toniocodo <toniocodo@hotmail.com>
2022-12-07 01:08:04 +0100

    feat: improve animation with variants, update layout

--- libs/mstable/shared/ui/src/Diagrams/MetavaultStrategy/components/AnimatedDiagram.tsx
+++ libs/mstable/shared/ui/src/Diagrams/MetavaultStrategy/components/AnimatedDiagram.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+
 import {
   MvUSDC,
   SimplePurpleBkgIcon,
@@ -5,7 +7,7 @@ import {
 } from '@frontend/shared-icons';
 import { Arrow, ContentContainer } from '@frontend/shared-ui';
 import { alpha, Stack, styled, Typography, useTheme } from '@mui/material';
-import { motion } from 'framer-motion';
+import { LayoutGroup, motion } from 'framer-motion';
 import { User, Vault } from 'phosphor-react';
 import { times } from 'ramda';
 import { useIntl } from 'react-intl';
@@ -39,27 +41,109 @@ export const MBox = styled(motion.div)(({ theme }) => ({
 
 const labelProps: TypographyProps = { variant: 'buttonSmall', noWrap: true };
 
-const transition: Transition = {
-  type: 'just',
-};
+const SIZES = [
+  ['20%', '60%', '20%'],
+  ['60%', '20%', '20%'],
+  ['20%', '60%', '20%'],
+  ['20%', '20%', '60%'],
+];
 
 export const AnimatedDiagram = (props: StackProps) => {
   const intl = useIntl();
   const theme = useTheme();
   const { step } = useSteps();
 
-  const sizes = [
-    ['20%', '60%', '20%'],
-    ['60%', '20%', '20%'],
-    ['20%', '60%', '20%'],
-    ['20%', '20%', '60%'],
-  ][step];
+  const currentSizes = useMemo(() => SIZES[step], [step]);
+
+  const widthTransition: Transition = {
+    type: 'spring',
+    bounce: 0,
+    duration: 0.5,
+  };
+
+  const widthVariants: Variants = {
+    '0': (i) => ({
+      width: SIZES[0][i],
+      transition: widthTransition,
+      opacity: 1,
+    }),
+    '1': (i) => ({
+      width: SIZES[1][i],
+      transition: widthTransition,
+      opacity: i === 0 ? 1 : 0.5,
+    }),
+    '2': (i) => ({
+      width: SIZES[2][i],
+      transition: widthTransition,
+      opacity: i === 1 ? 1 : 0.5,
+    }),
+    '3': (i) => ({
+      width: SIZES[3][i],
+      transition: widthTransition,
+      opacity: i === 2 ? 1 : 0.5,
+    }),
+  };
+
+  const underlyingVariants: Variants = {
+    open: {
+      opacity: 1,
+      display: 'block',
+      transition: {
+        type: 'spring',
+        bounce: 0,
+        duration: 0.5,
+        delayChildren: 0.2,
+        staggerChildren: 0.05,
+      },
+    },
+    closed: {
+      opacity: 0,
+      display: 'none',
+      transition: {
+        type: 'spring',
+        bounce: 0,
+        duration: 0.5,
+        delayChildren: 0.2,
+        staggerChildren: 0.05,
+      },
+    },
+  };
+
+  const underlyingItemVariants: Variants = {
+    open: {
+      opacity: 1,
+      transition: {
+        type: 'spring',
+        bounce: 0,
+      },
+    },
+    closed: {
+      opacity: 0,
+      transition: {
+        type: 'spring',
+        bounce: 0,
+      },
+    },
+  };
+
+  const metavaultContainerVariants: Variants = {
+    '0': { bottom: 0, borderRadius: '53px' },
+    '1': {
+      bottom: -180,
+      borderRadius: '8px',
+    },
+    '2': { bottom: 0, borderRadius: '53px' },
+    '3': {
+      bottom: -180,
+      borderRadius: '8px',
+    },
+  };
 
   const subArrowsContainerVariants: Variants = {
     open: (i) => ({
       opacity: 1,
       display: 'block',
-      width: sizes[i],
+      width: currentSizes[i],
       transition: {
         type: 'spring',
         bounce: 0,
@@ -78,29 +162,7 @@ export const AnimatedDiagram = (props: StackProps) => {
         duration: 0.5,
         delayChildren: 0.2,
         staggerChildren: 0.05,
-      },
-    },
-  };
-
-  const metavaultsVariants: Variants = {
-    open: {
-      opacity: 1,
-      y: 0,
-      display: 'block',
-      transition: {
-        type: 'spring',
-        bounce: 0,
-        duration: 0.05,
-      },
-    },
-    closed: {
-      opacity: 0,
-      y: 200,
-      display: 'none',
-      transition: {
-        type: 'spring',
-        bounce: 0,
-        duration: 0.05,
+        staggerDirection: -1,
       },
     },
   };
@@ -115,194 +177,189 @@ export const AnimatedDiagram = (props: StackProps) => {
 
   return (
     <Stack direction="column" alignItems="stretch" {...props}>
-      <Stack direction="row" justifyContent="center" alignItems="center">
-        <HBox
-          key="deposit"
-          layout
-          sx={{
-            justifyContent: 'flex-end',
-          }}
-          initial={false}
-          animate={{
-            opacity: [0, 1].includes(step) ? 1 : 0.5,
-            width: sizes[0],
-          }}
-          transition={transition}
-        >
-          <TriplePurpleBkgIcon icon={<User />} size={56} />
-          <Arrow flexGrow={1} mr={1} />
-          <ContentContainer accentBkg={[0, 1].includes(step)}>
-            <MvUSDC />
-            <Typography {...labelProps}>USDC</Typography>
-          </ContentContainer>
-          <Arrow flexGrow={1} />
-        </HBox>
-        <HBox
-          key="metavault"
-          layout
-          initial={false}
-          animate={{
-            opacity: [0, 2].includes(step) ? 1 : 0.7,
-            width: sizes[1],
-          }}
-          transition={transition}
-          style={{ position: 'relative', height: 34 }}
-        >
-          <ContentContainer
+      <LayoutGroup>
+        <Stack direction="row" justifyContent="center" alignItems="center">
+          <HBox
+            key="deposit"
             layout
-            accentBkg
+            sx={{
+              justifyContent: 'flex-end',
+            }}
+            initial={false}
+            animate={step.toString()}
+            variants={widthVariants}
+            custom={0}
+          >
+            <TriplePurpleBkgIcon icon={<User />} size={56} />
+            <Arrow flexGrow={1} mr={1} />
+            <ContentContainer accentBkg={[0, 1].includes(step)}>
+              <MvUSDC />
+              <Typography {...labelProps}>USDC</Typography>
+            </ContentContainer>
+            <Arrow flexGrow={1} />
+          </HBox>
+
+          <HBox
+            key="metavault"
+            layout
+            initial={false}
+            animate={step.toString()}
+            variants={widthVariants}
+            custom={1}
+            style={{ position: 'relative', height: 34 }}
+          >
+            <ContentContainer
+              layout
+              accentBkg
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                justifyContent: 'center',
+                margin: theme.spacing(0, 1),
+              }}
+              variants={metavaultContainerVariants}
+            >
+              <SimplePurpleBkgIcon icon={<Vault />} />
+              <Typography {...labelProps}>
+                {intl.formatMessage({ defaultMessage: 'Meta Vault' })}
+              </Typography>
+            </ContentContainer>
+          </HBox>
+
+          <HBox
+            key="withdraw"
+            layout
+            sx={{
+              justifyContent: 'flex-start',
+            }}
+            initial={false}
+            animate={step.toString()}
+            variants={widthVariants}
+            custom={2}
+          >
+            <Arrow flexGrow={1} mr={1} />
+            <ContentContainer accentBkg={[0, 3].includes(step)}>
+              <MvUSDC />
+              <Typography {...labelProps}>USDC</Typography>
+            </ContentContainer>
+            <Arrow flexGrow={1} mr={1} />
+            <TriplePurpleBkgIcon icon={<User />} size={56} />
+          </HBox>
+        </Stack>
+        <Stack direction="column" overflow="hidden">
+          <VBox
+            key="deposit-user"
+            layout
             style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              bottom: 0,
-              right: 0,
-              justifyContent: 'center',
-              margin: theme.spacing(0, 1),
+              alignSelf: 'flex-start',
             }}
-            animate={{
-              bottom: [1, 3].includes(step) ? -180 : 0,
-              borderRadius: [1, 3].includes(step) ? '8px' : '53px',
+            initial={{ opacity: 0, display: 'none' }}
+            animate={step === 1 ? 'open' : 'closed'}
+            variants={subArrowsContainerVariants}
+            custom={0}
+          >
+            {times(
+              (n) => (
+                <HBox key={`row-${n}`} variants={arrowItemVariants} custom={n}>
+                  <TriplePurpleBkgIcon icon={<User />} size={56} />
+                  <Arrow flexGrow={1} mr={1} />
+                  <ContentContainer accentBkg={[0, 1].includes(step)}>
+                    <MvUSDC />
+                    <Typography {...labelProps}>USDC</Typography>
+                  </ContentContainer>
+                  <Arrow flexGrow={1} />
+                </HBox>
+              ),
+              3,
+            )}
+          </VBox>
+
+          <HBox
+            key="underlying"
+            layout
+            initial={false}
+            animate={[0, 2].includes(step) ? 'open' : 'closed'}
+            variants={underlyingVariants}
+            style={{
+              alignSelf: 'center',
             }}
           >
-            <SimplePurpleBkgIcon icon={<Vault />} />
-            <Typography {...labelProps}>
-              {intl.formatMessage({ defaultMessage: 'Meta Vault' })}
-            </Typography>
-          </ContentContainer>
-        </HBox>
-        <HBox
-          key="withdraw"
-          layout
-          sx={{
-            justifyContent: 'flex-start',
-          }}
-          initial={false}
-          animate={{
-            opacity: [0, 3].includes(step) ? 1 : 0.7,
-            width: sizes[2],
-          }}
-          transition={transition}
-        >
-          <Arrow flexGrow={1} mr={1} />
-          <ContentContainer accentBkg={[0, 3].includes(step)}>
-            <MvUSDC />
-            <Typography {...labelProps}>USDC</Typography>
-          </ContentContainer>
-          <Arrow flexGrow={1} mr={1} />
-          <TriplePurpleBkgIcon icon={<User />} size={56} />
-        </HBox>
-      </Stack>
-      <Stack direction="column" overflow="hidden">
-        <VBox
-          key="deposit-user"
-          layout
-          style={{
-            alignSelf: 'flex-start',
-          }}
-          initial={{ opacity: 0, display: 'none' }}
-          animate={step === 1 ? 'open' : 'closed'}
-          variants={subArrowsContainerVariants}
-          custom={0}
-        >
-          {times(
-            (n) => (
-              <HBox key={`row-${n}`} variants={arrowItemVariants} custom={n}>
-                <TriplePurpleBkgIcon icon={<User />} size={56} />
-                <Arrow flexGrow={1} mr={1} />
-                <ContentContainer accentBkg={[0, 1].includes(step)}>
-                  <MvUSDC />
-                  <Typography {...labelProps}>USDC</Typography>
-                </ContentContainer>
-                <Arrow flexGrow={1} />
-              </HBox>
-            ),
-            3,
-          )}
-        </VBox>
+            <Stack
+              direction="row"
+              alignItems="center"
+              justifyContent="center"
+              spacing={5}
+            >
+              {underLyingVaults.map((v) => (
+                <VBox
+                  key={`${v.token.label}${v.vault.label}`}
+                  initial={false}
+                  variants={underlyingItemVariants}
+                  style={{ rowGap: 8, alignItems: 'center' }}
+                >
+                  <Stack direction="row">
+                    <Arrow direction="up" minHeight={50} />
+                    <Arrow direction="down" minHeight={50} />
+                  </Stack>
+                  <ContentContainer
+                    accentBkg={[0, 2].includes(step)}
+                    style={{ width: '100%', justifyContent: 'center' }}
+                  >
+                    {v.token.icon}
+                    <Typography {...labelProps} color="text.secondary">
+                      {v.token.label}
+                    </Typography>
+                  </ContentContainer>
+                  <Stack direction="row">
+                    <Arrow direction="up" minHeight={50} />
+                    <Arrow direction="down" minHeight={50} />
+                  </Stack>
+                  <ContentContainer
+                    square
+                    accentBkg={[0, 2].includes(step)}
+                    style={{ width: '100%' }}
+                  >
+                    {v.vault.icon}
+                    <Typography {...labelProps} color="text.secondary">
+                      {v.vault.label}
+                    </Typography>
+                  </ContentContainer>
+                </VBox>
+              ))}
+            </Stack>
+          </HBox>
 
-        <HBox
-          key="underlying"
-          layout
-          initial={{ opacity: 0, y: 200 }}
-          animate={[0, 2].includes(step) ? 'open' : 'closed'}
-          variants={metavaultsVariants}
-          style={{
-            alignSelf: 'center',
-          }}
-        >
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent="center"
-            spacing={1}
+          <VBox
+            layout
+            key="withdraw-users"
+            style={{
+              alignSelf: 'flex-end',
+            }}
+            initial={{ opacity: 0, width: 0 }}
+            animate={step === 3 ? 'open' : 'closed'}
+            variants={subArrowsContainerVariants}
+            custom={2}
           >
-            {underLyingVaults.map((v) => (
-              <Stack
-                key={`${v.token.label}${v.vault.label}`}
-                direction="column"
-                alignItems="center"
-                spacing={1}
-              >
-                <Stack direction="row">
-                  <Arrow direction="up" minHeight={50} />
-                  <Arrow direction="down" minHeight={50} />
-                </Stack>
-                <ContentContainer
-                  accentBkg={[0, 2].includes(step)}
-                  style={{ width: '100%', justifyContent: 'center' }}
-                >
-                  {v.token.icon}
-                  <Typography {...labelProps} color="text.secondary">
-                    {v.token.label}
-                  </Typography>
-                </ContentContainer>
-                <Stack direction="row">
-                  <Arrow direction="up" minHeight={50} />
-                  <Arrow direction="down" minHeight={50} />
-                </Stack>
-                <ContentContainer
-                  square
-                  accentBkg={[0, 2].includes(step)}
-                  style={{ width: '100%' }}
-                >
-                  {v.vault.icon}
-                  <Typography {...labelProps} color="text.secondary">
-                    {v.vault.label}
-                  </Typography>
-                </ContentContainer>
-              </Stack>
-            ))}
-          </Stack>
-        </HBox>
-
-        <VBox
-          layout
-          key="withdraw-users"
-          style={{
-            alignSelf: 'flex-end',
-          }}
-          initial={{ opacity: 0, width: 0 }}
-          animate={step === 3 ? 'open' : 'closed'}
-          variants={subArrowsContainerVariants}
-          custom={2}
-        >
-          {times(
-            (n) => (
-              <HBox key={`row-${n}`} variants={arrowItemVariants} custom={n}>
-                <Arrow flexGrow={1} mr={1} />
-                <ContentContainer accentBkg={[0, 1].includes(step)}>
-                  <MvUSDC />
-                  <Typography {...labelProps}>USDC</Typography>
-                </ContentContainer>
-                <Arrow flexGrow={1} mr={1} />
-                <TriplePurpleBkgIcon icon={<User />} size={56} />
-              </HBox>
-            ),
-            3,
-          )}
-        </VBox>
-      </Stack>
+            {times(
+              (n) => (
+                <HBox key={`row-${n}`} variants={arrowItemVariants} custom={n}>
+                  <Arrow flexGrow={1} mr={1} />
+                  <ContentContainer accentBkg={[0, 1].includes(step)}>
+                    <MvUSDC />
+                    <Typography {...labelProps}>USDC</Typography>
+                  </ContentContainer>
+                  <Arrow flexGrow={1} mr={1} />
+                  <TriplePurpleBkgIcon icon={<User />} size={56} />
+                </HBox>
+              ),
+              3,
+            )}
+          </VBox>
+        </Stack>
+      </LayoutGroup>
     </Stack>
   );
 };
