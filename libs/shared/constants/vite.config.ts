0c7b54dd78bb135f155e509ddc41ed49125983a8
toniocodo <toniocodo@hotmail.com>
2023-04-27 13:34:58 +0200

    fix: add svgr plugin

--- libs/shared/constants/vite.config.ts
+++ libs/shared/constants/vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { join } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
       skipDiagnostics: true,
     }),
     react(),
+    svgr(),
     tsconfigPaths({
       root: '../../../',
       projects: ['tsconfig.base.json'],
