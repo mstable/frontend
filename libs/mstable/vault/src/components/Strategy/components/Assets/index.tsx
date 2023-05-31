530fa091b9204db6aa44b0154f8983cd6670b2ea
Artsiom Yavorski <artsiom.yavorski@gmail.com>
2023-05-31 12:00:30 +0300

    feat: add asset icons and fund composition percentage

---
+++ libs/mstable/vault/src/components/Strategy/components/Assets/index.tsx
@@ -0,0 +1,65 @@
+import { AddressLabel } from '@frontend/shared-ui';
+import { TokenIconRevamp } from '@frontend/shared-ui';
+import { Box, Grid, Stack, Typography } from '@mui/material';
+import { useIntl } from 'react-intl';
+import { useNetwork } from 'wagmi';
+
+import { useAssetsComposition } from './hooks';
+
+import type { StackProps } from '@mui/material';
+
+export const Assets = (props: StackProps) => {
+  const intl = useIntl();
+  const { chain } = useNetwork();
+  const composition = useAssetsComposition();
+
+  return (
+    <Stack {...props} direction="column">
+      <Typography variant="body2" sx={{ color: 'text.secondary', pb: 2 }}>
+        {intl.formatMessage({
+          defaultMessage:
+            'The following assets are used in this strategy and therefore are exposed (partially) to their risk:',
+          id: 'korbaG',
+        })}
+      </Typography>
+      <Box>
+        <Grid container spacing={2}>
+          {composition?.map(
+            ({ tokenAddress, tokenName, asset, percentage }) => (
+              <Grid
+                item
+                key={`${tokenAddress}-${tokenName}`}
+                xs={6}
+                zeroMinWidth
+              >
+                <Stack
+                  direction="column"
+                  sx={{
+                    borderRadius: 1,
+                    border: (theme) => `1px solid ${theme.palette.divider}`,
+                    padding: 2,
+                  }}
+                >
+                  <TokenIconRevamp symbols={asset.iconSymbols} sx={{ mb: 2 }} />
+                  <Typography variant="h5" gutterBottom noWrap>
+                    {tokenName}
+                  </Typography>
+                  <AddressLabel
+                    small
+                    address={tokenAddress}
+                    link
+                    blockExplorerUrl={chain?.blockExplorers?.['etherscan']?.url}
+                    sx={{ maxWidth: 120 }}
+                  />
+                  <Typography variant="h6" gutterBottom noWrap>
+                    {percentage}
+                  </Typography>
+                </Stack>
+              </Grid>
+            ),
+          )}
+        </Grid>
+      </Box>
+    </Stack>
+  );
+};
--- libs/mstable/vault/src/views/Vault.tsx
+++ libs/mstable/vault/src/views/Vault.tsx
@@ -1,5 +1,6 @@
 import { useTrack } from '@frontend/shared-providers';
-import { ErrorBoundary, ErrorCard, MVIcon } from '@frontend/shared-ui';
+import { ErrorBoundary, ErrorCard } from '@frontend/shared-ui';
+import { TokenIconRevamp } from '@frontend/shared-ui';
 import { Button, Grid, Stack } from '@mui/material';
 import { useNavigate } from '@tanstack/react-location';
 import { ArrowLeft } from 'phosphor-react';
@@ -39,7 +40,10 @@ const VaultContent = ({ config, ...props }: VaultProps) => {
           {intl.formatMessage({ defaultMessage: 'Explore', id: '7JlauX' })}
         </Stack>
       </Button>
-      <MVIcon address={config.address} sx={{ height: 64, width: 64, mb: 2 }} />
+      <TokenIconRevamp
+        symbols={[config.symbol]}
+        sx={{ height: 64, width: 64, mb: 2 }}
+      />
       <ErrorBoundary
         ErrorComponent={
           <ErrorCard
