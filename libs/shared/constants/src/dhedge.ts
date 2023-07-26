79b5444b2bc539b5f525f21752dae020a8e0a887
Dmitry Lobachevsky <dmitriylobachevskiy@gmail.com>
2023-07-26 13:48:49 +0300

    feat: add dhedge link for vault assets in token composition

--- libs/shared/constants/src/dhedge.ts
+++ libs/shared/constants/src/dhedge.ts
@@ -1,3 +1,4 @@
 export const dHedgeApiEndpoint = 'https://api-v2.dhedge.org/graphql';
 
 export const torosDappLink = 'https://toros.finance';
+export const dHEDGEDappLink = 'https://app.dhedge.org';
