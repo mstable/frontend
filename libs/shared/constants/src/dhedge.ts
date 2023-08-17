c6519cec0ae253fafd02b10d2d1f577f0d4800f1
Dmitry Lobachevsky <dmitriylobachevskiy@gmail.com>
2023-08-17 10:19:31 +0300

    Revert "fix: dhedge api url"
    This reverts commit e77f598ecfc37003d3aaa8fa2cb09ccee90364c9.

--- libs/shared/constants/src/dhedge.ts
+++ libs/shared/constants/src/dhedge.ts
@@ -1,4 +1,4 @@
-export const dHedgeApiEndpoint = 'https://dev.dhedge.org/graphql';
+export const dHedgeApiEndpoint = 'https://api-v2.dhedge.org/graphql';
 
 export const torosDappLink = 'https://toros.finance';
 export const dHEDGEDappLink = 'https://app.dhedge.org';
