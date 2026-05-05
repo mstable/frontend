c2cf20a77261222f943890c98394619e22a52b67
Denis Ignatovich <denis.ignatovich.m@gmail.com>
2026-05-05 14:46:21 +0200

    Update l1ComptrollerContract & l2ComptrollerContract addresses

--- libs/dhedge/withdraw/src/constants/contracts.ts
+++ libs/dhedge/withdraw/src/constants/contracts.ts
@@ -7,14 +7,14 @@ import { arbitrum, mainnet } from 'wagmi/chains';
 import type { Contract } from '@frontend/shared-constants';
 
 export const l2ComptrollerContract: Contract = {
-  address: '0xa30ee837aE10Acb36fF75eA4a720E1fAa1BA2293',
+  address: '0x0659466338768dD2c648CdcE03EB26A9E7cc0369',
   chainId: arbitrum.id,
   abi: L2ComptrollerV2ABI,
   name: 'L2Comptroller',
 };
 
 export const l1ComptrollerContract: Contract = {
-  address: '0x53750692bB134C7de46f174d1CCB96E0c2270096',
+  address: '0xc87E1c912a346Db265616De90c95232D09A7F692',
   chainId: mainnet.id,
   abi: L1ComptrollerV2ABI,
   name: 'L1Comptroller',
